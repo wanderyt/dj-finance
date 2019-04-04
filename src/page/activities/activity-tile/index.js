e607961d694279abf155e99631f1dd13e4cec607
wanderyt <wanderyt1988@163.com>
2019-04-04 17:46:47 +0800

    Add act details page.

--- src/page/activities/activity-tile/index.js
+++ src/page/activities/activity-tile/index.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {formatDate} from '../util';
 
 import './index.scss';
+import ActivityDetails from './activity-details';
 
 const ActivityTile = ({id, name, desc, date, amount}) => {
+  let [isExpanded, setIsExpanded] = useState(false);
+  let [details, setDetails] = useState({});
+
+  const expandDetails = () => {
+    if (!details.items) {
+      const currentDetails = {
+        total: 900,
+        items: [{
+          name: '花',
+          amount: 300,
+        }, {
+          name: '花瓶',
+          amount: 400,
+        }, {
+          name: '包装纸',
+          amount: 200,
+        }]
+      };
+
+      setDetails(currentDetails);
+    }
+    setIsExpanded(true);
+  };
+
+  const foldItem = () => {
+    setIsExpanded(false);
+  }
+
   return (
     <div className='ActivityTile'>
       <div className='Activity--Item Activity--Header'>
@@ -17,8 +46,27 @@ const ActivityTile = ({id, name, desc, date, amount}) => {
         <div className='Activity--AmountCaption'>花费金额：</div>
         <div className='Activity--AmountNumber'>{amount}</div>
       </div>
+      {
+        isExpanded && details &&
+        <div className='Activity--Item'>
+          <ActivityDetails details={details} />
+        </div>
+      }
       <div className='Activity--Item Activity--Toolbar'>
-        <div className='Activity--LearnMore'>查看更多</div>
+        {
+          !isExpanded ?
+          <div
+            className='Activity--LearnMore'
+            onClick={expandDetails}>
+            查看明细
+          </div>
+          :
+          <div
+            className='Activity--Fold'
+            onClick={foldItem}>
+            收起
+          </div>
+        }
       </div>
     </div>
   );
