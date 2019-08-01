e0b38fbe30acd7f98211568ac4713dc9fb363a4c
wanderyt <wanderyt1988@163.com>
2019-08-01 15:53:07 +0800

    Add fix for other routes to be valid.

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const path = require('path');
 app.use(express.static('build'));
 
 // main route
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './build/index.html'));
 });
 
