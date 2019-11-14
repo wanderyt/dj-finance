b51022a5aae66605eb232091a551b1d3136ba984
David, Ren <wanderyt1988@163.com>
2019-11-14 10:24:50 +0800

    Add build resources. (#26)

---
+++ build/precache-manifest.b509a78e1b7e01bfdac9fbaec63506d3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7cc064968c57b78a767",
+    "url": "/static/js/page-wheather.a4d7f0ca.chunk.js"
+  },
+  {
+    "revision": "541ad8db83cae8a6a477",
+    "url": "/static/css/main.ba53af75.chunk.css"
+  },
+  {
+    "revision": "8d9790cb480d107206d5",
+    "url": "/static/css/page-activities.609b0c83.chunk.css"
+  },
+  {
+    "revision": "8d9790cb480d107206d5",
+    "url": "/static/js/page-activities.d2c9eb99.chunk.js"
+  },
+  {
+    "revision": "028c8f01338ce596f3e9",
+    "url": "/static/js/page-charts.0fa37901.chunk.js"
+  },
+  {
+    "revision": "e53e59d672ee36a5255c",
+    "url": "/static/js/page-testImages.724b696f.chunk.js"
+  },
+  {
+    "revision": "de1aa14f042dcc1f08a4",
+    "url": "/static/js/page-testImagesLazyload.4917ffd2.chunk.js"
+  },
+  {
+    "revision": "96f1cc08572343c583b6",
+    "url": "/static/js/page-weather.4db4e430.chunk.js"
+  },
+  {
+    "revision": "541ad8db83cae8a6a477",
+    "url": "/static/js/main.6c365fb8.chunk.js"
+  },
+  {
+    "revision": "570f01349bdc02e13c58",
+    "url": "/static/js/runtime~main.2eea2da1.js"
+  },
+  {
+    "revision": "bd454f5dbbc708a19973",
+    "url": "/static/js/8.c8b82b87.chunk.js"
+  },
+  {
+    "revision": "71ded62a1d05db7607ca",
+    "url": "/static/js/9.94b42cbf.chunk.js"
+  },
+  {
+    "revision": "630d8c647c240503486c",
+    "url": "/static/js/10.92bfbb6b.chunk.js"
+  },
+  {
+    "revision": "547e7245e8005fff83c07c8974a18519",
+    "url": "/static/media/Spinner.547e7245.svg"
+  },
+  {
+    "revision": "373e500570593ad15aca1151c8c249e3",
+    "url": "/static/media/arrow.373e5005.svg"
+  },
+  {
+    "revision": "1bead6abcfb00a3fc9b09a94560f5dd9",
+    "url": "/index.html"
+  }
+];
